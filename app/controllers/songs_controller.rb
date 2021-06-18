48f680ea7ee2b193cf5d07cf0370ef4852a263d9
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 15:50:15 -0400

    Archive Flatiron code.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,26 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      begin
+        @songs = Artist.find(params[:artist_id]).songs 
+      rescue ActiveRecord::RecordNotFound
+        redirect_to artists_path, alert: "Artist not found."
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      begin
+        @song = Song.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        redirect_to artist_songs_path(Artist.find(params[:artist_id])), alert: "Song not found."
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def new
